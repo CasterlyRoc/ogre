154ed374a9d1112321ddb4c8545849755c012c16
Zachery Knopp <zknopp@Zacherys-MacBook-Pro.local>
2015-04-10 16:06:26 -0400

    File i/o update

--- Node.rb
+++ Node.rb
@@ -29,13 +29,98 @@ class Node
 
 	attr_accessor:name,:ip_addrs,:adj_hash
 
-	def initialize(name){
-		
-	}
+	def initialize(name)
+		@name = name
+		@ip_addrs = Array.new
+		@adj_hash = Hash.new
+	end
 
+	def add_ip_addr(ip)
+		ip_addrs.push(ip)
+	end
+
+	def add_neighbor(dest_node, cost)
+		adj_hash.store(dest_node, cost)
+	end
+
+	def to_s
+		puts "Node: #{name}"
+		puts "---IP Addresses---"
+		ip_addrs.each{ |s|
+			puts "  " + s
+		}
+		puts "---Neighbors---"
+		adj_hash.each{ |k,v|
+			puts "  has an edge to #{k} with cost #{v}"
+		}
+	end
 end
 
-# Execute hostname to get name
-# Look up node's IP address with name
-# Find neighbors and cost
+# Execute hostname to get name of the node
+name_of_node = `hostname`
+node = Node.new("n1")
+
+# Process config file
+
+config_file = open(ARGV[0])
+
+node_line = config_file.gets
+node_line.chomp!
+nodes_to_addr_file = open(node_line)
+
+link_line = config_file.gets
+link_line.chomp!
+link_file = open(link_line)
+
+# Get ip addresses assoc with the node
+while nodes_to_addr_line = nodes_to_addr_file.gets
+	name_of_node, ip_addr = nodes_to_addr_line.split(" ")
+	if(name_of_node == node.name)
+		node.add_ip_addr(ip_addr)
+	end
+end
+
+# Get links between nodes and the cost
+while link_line = link_file.gets
+	source_node, dest_node, cost = link_line.split(" ")
+	if(node.ip_addrs.include?(source_node))
+		c = cost.to_i
+		node.add_neighbor(dest_node, c)
+	end
+end
+
+node.to_s
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
